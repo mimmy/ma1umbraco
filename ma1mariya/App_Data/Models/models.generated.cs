2f9fe693ad7e7ef82b69c9eb5ea45a2d06d211c0
Mariya <dimitrova@jurato.de>
2016-11-13 18:40:45 +0100

    most is there

--- ma1mariya/App_Data/Models/models.generated.cs
+++ ma1mariya/App_Data/Models/models.generated.cs
@@ -20,7 +20,7 @@ using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
 [assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "10d7297ef6dff8b7")]
-[assembly:System.Reflection.AssemblyVersion("0.0.0.2")]
+[assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
 {
